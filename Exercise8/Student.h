5127b3bfc6f8e653ff401d005fa1bdcd3ae3d7ae
Sebastian Sopola <98320995+2022IsHere@users.noreply.github.com>
2023-04-05 11:39:51 +0300

    Memory allocation, Structs, FILE I/0, Struct modify

---
+++ Exercise8/Student.h
@@ -0,0 +1,44 @@
+// File:	Student.h
+// Author(s): Sebastian Sopola 		
+//Description: Header file for Student.h 
+ 
+
+#ifndef STUDENT_H
+#define STUDENT_H
+
+// built-in libraries for specific use
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <math.h>
+
+// determine size of student name
+#define NAMESIZE 31
+
+// determine size of user answer
+#define ANSWERSIZE 5 
+
+// determine student id size
+#define IDSIZE 7
+
+
+
+
+
+// declare functions we wish to share for other files to use
+bool presentChoice();
+int totalStudents();
+struct student * createArray( int totalStudents );  
+int askStudentInformation();	
+int writeStudentsToFile();
+void showStudentInformation();
+int changeStudentInfo();
+float averageGpa(struct student *memoryPointer, int totalStudents);
+void showAverage(float average);
+void readFromFile();
+void testFunction();
+
+
+
+#endif 
